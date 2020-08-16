d7a61039e37a3f23bdb916b8f6f5a018524d3751
sarah <limssarah4@gmail.com>
2020-08-17 00:10:29 +0200

    allocine

--- src/components/pagination.jsx
+++ src/components/pagination.jsx
@@ -7,18 +7,29 @@ class pagination extends React.Component{
 
           return (
             
-           <div className="pagination">
-             <nav aria-label="Page navigation example">
-                <ul class="pagination">
-                    <li class="page-item"><a class="page-link" onClick={()=>previous()}>Previous</a></li>
-                    <li class="page-item"><a class="page-link" href="#">1</a></li>
-                    <li class="page-item"><a class="page-link" href="#">2</a></li>
-                    <li class="page-item"><a class="page-link" href="#">3</a></li>
-                    <li class="page-item"><a class="page-link" onClick={()=>next()}>Next</a></li>
-                </ul>
-             </nav>
-           </div>         
-        
+            <div>
+            <div className="pagination1">
+       <nav aria-label="Page navigation example">
+          <ul class="pagination">
+    <li class="page-item"><a class="page-link" onClick={()=>previous()}>P</a></li>
+              <li class="page-item"><a class="page-link" href="#">1</a></li>
+              <li class="page-item"><a class="page-link" onClick={()=>next()}>N</a></li>
+          </ul>
+       </nav>
+     </div>    
+     <div className="pagination2">
+       <nav aria-label="Page navigation example">
+          <ul class="pagination">
+              <li class="page-item"><a class="page-link" onClick={()=>previous()}>Previous</a></li>
+              <li class="page-item"><a class="page-link" href="#">1</a></li>
+              <li class="page-item"><a class="page-link" href="#">2</a></li>
+              <li class="page-item"><a class="page-link" href="#">3</a></li>
+              <li class="page-item"><a class="page-link" onClick={()=>next()}>Next</a></li>
+          </ul>
+       </nav>
+     </div>    
+        </div>
+      
             
         )
     }
